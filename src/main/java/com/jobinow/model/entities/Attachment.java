10b02175f8f3d30483cfb97aac69c7420552b8c5
ouharri <ouharrioutman@gmail.com>
2024-01-04 13:30:06 +0000

    Represents an attachment entity in the system. This class includes information about an attachment, such as a cover letter, associated with a job application.

--- src/main/java/com/jobinow/model/entities/Attachment.java
+++ src/main/java/com/jobinow/model/entities/Attachment.java
@@ -9,6 +9,12 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.validator.constraints.URL;
 
+/**
+ * Represents an attachment entity in the system.
+ * This class includes information about an attachment, such as a cover letter, associated with a job application.
+ *
+ * @version 1.0
+ */
 @Getter
 @Setter
 @Entity
@@ -16,12 +22,22 @@ import org.hibernate.validator.constraints.URL;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Attachment extends AbstractEntity {
-    @URL
+
+    /**
+     * The URL to the cover letter attachment.
+     */
+    @URL(message = "Attachment URL must be valid")
     private String letter;
 
+    /**
+     * The job application to which the attachment belongs.
+     */
     @ManyToOne
     private Apply apply;
 
+    /**
+     * The company associated with the attachment.
+     */
     @ManyToOne
     private Company company;
 }
