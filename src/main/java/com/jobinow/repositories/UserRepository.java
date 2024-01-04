217063ff57e01312bc8ada13b7da2d7a26f005dd
ouharri <ouharrioutman@gmail.com>
2024-01-04 15:54:37 +0000

    Repository interface for accessing and managing User entities in the database. This repository provides methods to interact with user entities, such as finding users by email or ID, and retrieving users based on their status.

--- src/main/java/com/jobinow/repositories/UserRepository.java
+++ src/main/java/com/jobinow/repositories/UserRepository.java
@@ -1,15 +1,22 @@
 package com.jobinow.repositories;
 
 import com.jobinow.model.entities.User;
+import com.jobinow.model.enums.UserStatus;
 import io.micrometer.common.lang.NonNullApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Repository interface for accessing and managing {@link User} entities in the database.
+ *
+ * <p>This repository provides methods to interact with user entities, such as finding users by
+ * email or ID, and retrieving users based on their status.</p>
+ *
+ * @version 1.0
  */
 @Repository
 @NonNullApi
@@ -30,4 +37,12 @@ public interface UserRepository extends JpaRepository<User, UUID> {
      * @return An optional containing the user, or empty if not found.
      */
     Optional<User> findById(UUID id);
+
+    /**
+     * Finds all users with a specific status.
+     *
+     * @param status The status of the users to retrieve.
+     * @return A list of users with the specified status.
+     */
+    List<User> findAllByStatus(UserStatus status);
 }
