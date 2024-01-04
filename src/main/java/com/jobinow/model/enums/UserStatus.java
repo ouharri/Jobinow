987fb2be735e8f56be8fad9fe1c085fc7e9875ab
ouharri <ouharrioutman@gmail.com>
2024-01-04 15:00:08 +0000

    Enumeration represents the status of a user (e.g., online or offline). Use this enum to indicate the current status of a user within the application. It provides constants for online and offline status.

---
+++ src/main/java/com/jobinow/model/enums/UserStatus.java
@@ -0,0 +1,21 @@
+package com.jobinow.model.enums;
+
+/**
+ * Enumeration representing the status of a user (e.g., online or offline).
+ *
+ * <p>Use this enum to indicate the current status of a user within the application.
+ * It provides constants for online and offline status.</p>
+ *
+ * @version 1.0
+ */
+public enum UserStatus {
+    /**
+     * Represents the status when a user is online.
+     */
+    ONLINE,
+
+    /**
+     * Represents the status when a user is offline.
+     */
+    OFFLINE
+}
