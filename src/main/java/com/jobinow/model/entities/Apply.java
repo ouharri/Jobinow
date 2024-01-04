aa8391f7082e0fac9c3f5fa1eeb143043b81905c
ouharri <ouharrioutman@gmail.com>
2024-01-04 13:29:53 +0000

    Represents a job application entity in the system. This class includes information about a job application, such as the job seeker, associated job offer, and resume attachments.

--- src/main/java/com/jobinow/model/entities/Apply.java
+++ src/main/java/com/jobinow/model/entities/Apply.java
@@ -11,6 +11,13 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.Set;
 
+/**
+ * Represents a job application entity in the system.
+ * This class includes information about a job application,
+ * such as the job seeker, associated job offer, and resume attachments.
+ *
+ * @version 1.0
+ */
 @Getter
 @Setter
 @Entity
@@ -18,12 +25,24 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Apply extends AbstractEntity {
+
+    /**
+     * The job seeker who submitted the application.
+     */
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     private User jobSeeker;
+
+    /**
+     * The job offer to which the application is submitted.
+     */
     @ManyToOne
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Offre offre;
+
+    /**
+     * The set of resume attachments (PDFs) associated with the application.
+     */
     @OneToMany
     private Set<Attachment> resumePdfs;
 }
