df9e77fa0ce90bdb57ecce101b353bd2e64fc97d
ouharri <ouharrioutman@gmail.com>
2024-01-05 08:49:55 +0000

    Service implementation for managing chat rooms. This service provides methods for getting chat room IDs based on sender and recipient IDs. It can also create a new chat room if it does not exist.

--- src/main/java/com/jobinow/services/impl/ChatRoomServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/ChatRoomServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jobinow.services.impl;
 
 import com.jobinow.model.entities.ChatRoom;
+import com.jobinow.model.entities.User;
 import com.jobinow.repositories.ChatRoomRepository;
 import com.jobinow.services.spec.ChatRoomService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Service implementation for managing chat rooms.
@@ -26,22 +28,25 @@ public class ChatRoomServiceImpl implements ChatRoomService {
     /**
      * Gets the chat room ID for the given sender and recipient IDs.
      *
-     * @param senderId                The ID of the sender.
-     * @param recipientId             The ID of the recipient.
+     * @param sender                   The ID of the sender.
+     * @param recipient                The ID of the recipient.
      * @param createNewRoomIfNotExists If true, creates a new chat room if it does not exist.
      * @return An optional containing the chat room ID, or empty if not found or creation is disabled.
      */
     public Optional<String> getChatRoomId(
-            String senderId,
-            String recipientId,
+            User sender,
+            User recipient,
             boolean createNewRoomIfNotExists
     ) {
         return chatRoomRepository
-                .findBySenderIdAndRecipientId(senderId, recipientId)
+                .findBySenderAndRecipient(sender, recipient)
                 .map(ChatRoom::getChatId)
                 .or(() -> {
                     if (createNewRoomIfNotExists) {
-                        var chatId = createChatId(senderId, recipientId);
+                        var chatId = createChatId(
+                                sender,
+                                recipient
+                        );
                         return Optional.of(chatId);
                     }
 
@@ -49,21 +54,89 @@ public class ChatRoomServiceImpl implements ChatRoomService {
                 });
     }
 
-    private String createChatId(String senderId, String recipientId) {
-        var chatId = String.format("%s_%s", senderId, recipientId);
+    public Optional<String> getChatRoomId(
+            UUID sender,
+            UUID recipient,
+            boolean createNewRoomIfNotExists
+    ) {
+        return chatRoomRepository
+                .findBySender_IdAndRecipient_Id(sender, recipient)
+                .map(ChatRoom::getChatId)
+                .or(() -> {
+                    if (createNewRoomIfNotExists) {
+                        var chatId = createChatId(
+                                sender,
+                                recipient
+                        );
+                        return Optional.of(chatId);
+                    }
+
+                    return Optional.empty();
+                });
+    }
+
+    private String createChatId(User sender, User recipient) {
+        var chatId = String.format(
+                "%s_%s",
+                sender.getId(),
+                recipient.getId()
+        );
 
         ChatRoom senderRecipient = ChatRoom
                 .builder()
                 .chatId(chatId)
-                .senderId(senderId)
-                .recipientId(recipientId)
+                .sender(sender)
+                .recipient(recipient)
                 .build();
 
         ChatRoom recipientSender = ChatRoom
                 .builder()
                 .chatId(chatId)
-                .senderId(recipientId)
-                .recipientId(senderId)
+                .sender(recipient)
+                .recipient(sender)
+                .build();
+
+        chatRoomRepository.save(senderRecipient);
+        chatRoomRepository.save(recipientSender);
+
+        return chatId;
+    }
+
+    private String createChatId(UUID sender, UUID recipient) {
+        var chatId = String.format(
+                "%s_%s",
+                sender.toString(),
+                recipient.toString()
+        );
+
+        ChatRoom senderRecipient = ChatRoom
+                .builder()
+                .chatId(chatId)
+                .sender(
+                        User.builder()
+                                .id(sender)
+                                .build()
+                )
+                .recipient(
+                        User.builder()
+                                .id(recipient)
+                                .build()
+                )
+                .build();
+
+        ChatRoom recipientSender = ChatRoom
+                .builder()
+                .chatId(chatId)
+                .sender(
+                        User.builder()
+                                .id(recipient)
+                                .build()
+                )
+                .recipient(
+                        User.builder()
+                                .id(sender)
+                                .build()
+                )
                 .build();
 
         chatRoomRepository.save(senderRecipient);
