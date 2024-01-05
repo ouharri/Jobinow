406f1a82c35d77584a7047c49f7c574de4e56a21
ouharri <ouharrioutman@gmail.com>
2024-01-05 08:48:44 +0000

    Service interface for managing chat rooms between users. This interface provides methods to get a chat room ID based on sender and recipient IDs. It also allows creating a new chat room if it does not exist.

--- src/main/java/com/jobinow/services/spec/ChatRoomService.java
+++ src/main/java/com/jobinow/services/spec/ChatRoomService.java
@@ -1,6 +1,9 @@
 package com.jobinow.services.spec;
 
+import com.jobinow.model.entities.User;
+
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * Service interface for managing chat rooms between users.
@@ -15,14 +18,20 @@ public interface ChatRoomService {
     /**
      * Gets the chat room ID based on sender and recipient IDs.
      *
-     * @param senderId               The ID of the sender user.
-     * @param recipientId            The ID of the recipient user.
+     * @param senderId                 The ID of the sender user.
+     * @param recipientId              The ID of the recipient user.
      * @param createNewRoomIfNotExists If true, creates a new room if it does not exist.
      * @return An optional containing the chat room ID if it exists.
      */
     Optional<String> getChatRoomId(
-            String senderId,
-            String recipientId,
+            User sender,
+            User recipient,
+            boolean createNewRoomIfNotExists
+    );
+
+    Optional<String> getChatRoomId(
+            UUID sender,
+            UUID recipient,
             boolean createNewRoomIfNotExists
     );
 
