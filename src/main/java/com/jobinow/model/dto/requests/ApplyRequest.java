a2febf9aefa8c91d81df532e0c5c754f31b3c5d6
ouharri <ouharrioutman@gmail.com>
2024-01-05 16:27:30 +0000

    DTO Request for Apply Represents a request data transfer object for creating or updating an Apply. It encapsulates the necessary information for applying to a job offer, including details about the job seeker, the target job offer, and a set of attachment URLs.

--- src/main/java/com/jobinow/model/dto/requests/ApplyRequest.java
+++ src/main/java/com/jobinow/model/dto/requests/ApplyRequest.java
@@ -1,6 +1,5 @@
 package com.jobinow.model.dto.requests;
 
-import com.jobinow.model.dto.responses.AttachmentResponse;
 import com.jobinow.model.dto.responses.OfferResponse;
 import com.jobinow.model.dto.responses.UserResponses;
 import com.jobinow.model.entities.Apply;
@@ -9,10 +8,14 @@ import java.util.Set;
 
 /**
  * DTO Request for {@link Apply}
+ * <p>
+ * Represents a request data transfer object for creating or updating an {@link Apply}.
+ * It encapsulates the necessary information for applying to a job offer, including details
+ * about the job seeker, the target job offer, and a set of attachment URLs.
  */
 public record ApplyRequest(
         UserResponses jobSeeker,
         OfferResponse Offer,
-        Set<AttachmentResponse> attachmentUrls
+        Set<AttachmentRequest> attachmentUrls
 ) implements _Request {
 }
