6ab6089001010099a4144d5942a6d91cda620cb8
ouharri <ouharrioutman@gmail.com>
2024-01-05 13:02:50 +0000

    DTO for creating or updating a User entity. Represents a user request with details such as password, image, phone number, email, first name, last name, gender, and address.

---
+++ src/main/java/com/jobinow/model/dto/requests/UserRequest.java
@@ -0,0 +1,34 @@
+package com.jobinow.model.dto.requests;
+
+import com.jobinow.model.dto.basic.AddressDto;
+import com.jobinow.model.entities.User;
+import com.jobinow.model.enums.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+/**
+ * DTO for creating or updating a {@link User} entity.
+ * Represents a user request with details such as password, image, phone number,
+ * email, first name, last name, gender, and address.
+ */
+public record UserRequest(
+        String password,
+        String image,
+
+        @Pattern(message = "Phone number must match the format '0XXXXXXXXX'", regexp = "0\\d{9}")
+        String phoneNumber,
+
+        @Size(message = "Email is too long", max = 80) @Email(message = "Email was not provided")
+        String email,
+
+        @NotNull(message = "FirstName must be present") @Size(message = "Firstname cannot be empty", min = 1)
+        String firstname,
+        @Size(message = "Lastname is too long", max = 30)
+        String lastname,
+        Gender gender,
+
+        AddressDto address
+) implements _Request {
+}
