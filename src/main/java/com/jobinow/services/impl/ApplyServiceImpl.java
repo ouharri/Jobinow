feb12e39d8b3884d712b026653e4fa31a15c2ae3
ouharri <ouharrioutman@gmail.com>
2024-01-05 10:00:07 +0000

    ApplyServiceImpl

---
+++ src/main/java/com/jobinow/services/impl/ApplyServiceImpl.java
@@ -0,0 +1,15 @@
+package com.jobinow.services.impl;
+
+import com.jobinow.model.entities.Apply;
+import com.jobinow.services.spec.ApplyService;
+
+public class ApplyServiceImpl implements ApplyService {
+    /**
+     * @param apply
+     * @return
+     */
+    @Override
+    public Apply create(Apply apply) {
+        return null;
+    }
+}
