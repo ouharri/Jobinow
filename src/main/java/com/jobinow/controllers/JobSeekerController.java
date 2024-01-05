5fd5df0a25c6ae66bb413bae945e50ed58a11895
ouharri <ouharrioutman@gmail.com>
2024-01-05 13:07:35 +0000

    JobSeekerController

--- src/main/java/com/jobinow/controllers/JobSeekerController.java
+++ src/main/java/com/jobinow/controllers/JobSeekerController.java
@@ -1,4 +1,17 @@
 package com.jobinow.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/JobSeeker")
 public class JobSeekerController {
+
+
 }
