1a49cd74a605d264b09e260896e574384e561cc7
ouharri <ouharrioutman@gmail.com>
2024-01-06 22:36:16 +0000

    Implementation of the ApplyService interface providing functionality for applying to job offers. This class extends the generic service implementation _ServiceImp.

--- src/main/java/com/jobinow/services/impl/ApplyServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/ApplyServiceImpl.java
@@ -1,20 +1,22 @@
 package com.jobinow.services.impl;
 
 import com.jobinow.model.dto.requests.ApplyRequest;
-import com.jobinow.model.dto.requests.CompanyRequest;
 import com.jobinow.model.dto.responses.ApplyResponse;
-import com.jobinow.model.dto.responses.CompanyResponse;
 import com.jobinow.model.entities.Apply;
-import com.jobinow.model.entities.Company;
 import com.jobinow.model.mapper.ApplyMapper;
-import com.jobinow.model.mapper.CompanyMapper;
 import com.jobinow.repositories.ApplyRepository;
-import com.jobinow.repositories.CompanyRepository;
 import com.jobinow.services.spec.ApplyService;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * Implementation of the {@link ApplyService} interface providing functionality for applying to job offers.
+ * This class extends the generic service implementation {@link _ServiceImp}.
+ *
+ * @see ApplyService
+ * @see _ServiceImp
+ */
 @Service
 public class ApplyServiceImpl extends _ServiceImp<UUID, ApplyRequest, ApplyResponse, Apply, ApplyRepository, ApplyMapper> implements ApplyService {
 
