3745aeb459794340d9f563d76c81ed1cc5419756
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:25:34 +0000

    Service interface for managing job profiles. This service extends the generic service interface _Service, specializing in operations related to job profile entities. It defines methods for creating, retrieving, and handling other aspects of job profile data. The interface is designed to work with UUID as the identifier type for job profile entities.

--- src/main/java/com/jobinow/services/spec/ProfileService.java
+++ src/main/java/com/jobinow/services/spec/ProfileService.java
@@ -5,5 +5,20 @@ import com.jobinow.model.dto.responses.ProfilResponse;
 
 import java.util.UUID;
 
+/**
+ * Service interface for managing job profiles.
+ * This service extends the generic service interface {@link _Service},
+ * specializing in operations related to job profile entities.
+ * It defines methods for creating, retrieving, and handling
+ * other aspects of job profile data.
+ * <p>
+ * The interface is designed to work with UUID as the identifier type for job profile entities.
+ * </p>
+ *
+ * @version 1.0
+ * @see ProfilRequest
+ * @see ProfilResponse
+ * @see _Service
+ */
 public interface ProfileService extends _Service<UUID, ProfilRequest, ProfilResponse> {
 }
