48f590b6362ac5ec39ad07fbffc9655e94532a46
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:27:43 +0000

    Service implementation class for managing job offers. This service provides methods for creating, updating, and retrieving operations related to Offer. It extends the generic service implementation (_ServiceImp) and leverages the OfferMapper for mapping between entities and DTOs. The class handles operations such as creating, updating, and retrieving job offers, using the specified repository (OfferRepository) for data access.

--- src/main/java/com/jobinow/services/impl/OfferServiceImp.java
+++ src/main/java/com/jobinow/services/impl/OfferServiceImp.java
@@ -12,6 +12,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * Service implementation class for managing job offers.
+ * This service provides methods for creating, updating, and retrieving operations related to {@link Offer}.
+ * It extends the generic service implementation ({@link _ServiceImp}) and leverages the {@link OfferMapper}
+ * for mapping between entities and DTOs.
+ * <p>
+ * The class handles operations such as creating, updating, and retrieving job offers, using the specified
+ * repository ({@link OfferRepository}) for data access.
+ * </p>
+ *
+ * @author <a href="mailto:ouharrioutman@gmail.com">ouharri outman</a>
+ * @version 1.0
+ * @see _ServiceImp
+ * @see Offer
+ * @see OfferRequest
+ * @see OfferResponse
+ * @see OfferMapper
+ * @see OfferRepository
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
