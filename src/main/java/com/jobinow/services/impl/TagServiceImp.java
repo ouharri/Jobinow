18fe02123b886008438c52cebada0cb562a6e83b
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:28:03 +0000

    Service implementation class for managing tags. This service provides methods for creating, updating, and retrieving operations related to Tag. It extends the generic service implementation (_ServiceImp) and leverages the TagMapper for mapping between entities and DTOs.

--- src/main/java/com/jobinow/services/impl/TagServiceImp.java
+++ src/main/java/com/jobinow/services/impl/TagServiceImp.java
@@ -12,6 +12,25 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * Service implementation class for managing tags.
+ * This service provides methods for creating, updating, and retrieving operations related to {@link Tag}.
+ * It extends the generic service implementation ({@link _ServiceImp}) and leverages the {@link TagMapper}
+ * for mapping between entities and DTOs.
+ * <p>
+ * The class handles operations such as creating, updating, and retrieving tags, using the specified
+ * repository ({@link TagRepository}) for data access.
+ * </p>
+
+ *
+ * @version 1.0
+ * @see _ServiceImp
+ * @see Tag
+ * @see TagRequest
+ * @see TagResponse
+ * @see TagMapper
+ * @see TagRepository
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
