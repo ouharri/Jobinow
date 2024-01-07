e7d86afcdbd44c2f3221e75e1c608fd8ea9c2dd2
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:28:38 +0000

    Service implementation class for managing verification codes. This service provides methods for creating and validating verification codes. It handles operations related to VerificationCode, such as creating and checking the validity of verification codes associated with a specific Company

--- src/main/java/com/jobinow/services/impl/VerificationCodeServiceImp.java
+++ src/main/java/com/jobinow/services/impl/VerificationCodeServiceImp.java
@@ -2,6 +2,7 @@ package com.jobinow.services.impl;
 
 import com.jobinow.exceptions.ResourceNotCreatedException;
 import com.jobinow.model.dto.requests.VerificationCodeRequest;
+import com.jobinow.model.entities.Company;
 import com.jobinow.model.entities.VerificationCode;
 import com.jobinow.model.mapper.VerificationCodeMapper;
 import com.jobinow.repositories.VerificationCodeRepository;
@@ -10,8 +11,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
+/**
+ * Service implementation class for managing verification codes.
+ * This service provides methods for creating and validating verification codes.
+ * It handles operations related to {@link VerificationCode}, such as creating and checking the validity
+ * of verification codes associated with a specific {@link Company}.
+ * <p>
+ * The class uses the specified repository ({@link VerificationCodeRepository}) for data access
+ * and leverages the {@link VerificationCodeMapper} for mapping between entities and DTOs.
+ * </p>
+ *
+ * @version 1.0
+ * @see VerificationCodeService
+ * @see VerificationCode
+ * @see VerificationCodeRequest
+ * @see VerificationCodeMapper
+ * @see VerificationCodeRepository
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -20,15 +36,27 @@ public class VerificationCodeServiceImp implements VerificationCodeService {
     private final VerificationCodeMapper mapper;
     private final VerificationCodeRepository repository;
 
-    public Optional<VerificationCode> createVerificationCode(VerificationCode verificationCode) {
+    /**
+     * Creates a new verification code in the repository.
+     *
+     * @param verificationCode The verification code to be created
+     * @throws ResourceNotCreatedException If an error occurs while creating the verification code
+     */
+    public void createVerificationCode(VerificationCode verificationCode) {
         try {
-            return Optional.of(repository.saveAndFlush(verificationCode));
+            repository.saveAndFlush(verificationCode);
         } catch (Exception e) {
             log.error("Error while creating VerificationCode", e);
             throw new ResourceNotCreatedException(e.getMessage());
         }
     }
 
+    /**
+     * Checks the validity of a verification code.
+     *
+     * @param verificationCode The verification code to be validated
+     * @return {@code true} if the verification code is valid, {@code false} otherwise
+     */
     public Boolean isValidVerificationCode(VerificationCodeRequest verificationCode) {
         VerificationCode verificationCodeToCreate = mapper.toEntityFromRequest(verificationCode);
         return repository.isValidVerificationCode(
