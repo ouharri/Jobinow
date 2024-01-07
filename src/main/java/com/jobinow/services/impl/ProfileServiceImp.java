fbb71c78d424e6273ceba46bc6214b43704a0bbf
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:30:23 +0000

    Implementation of the JobSeekerService interface for job seeker-related operations. This service provides methods for job seekers to apply to jobs, retrieve applied, saved, and recommended jobs, and filter jobs based on various criteria such as profile, location, salary range, job title, and company.

--- src/main/java/com/jobinow/services/impl/ProfileServiceImp.java
+++ src/main/java/com/jobinow/services/impl/ProfileServiceImp.java
@@ -2,9 +2,11 @@ package com.jobinow.services.impl;
 
 import com.jobinow.model.dto.requests.ProfilRequest;
 import com.jobinow.model.dto.responses.ProfilResponse;
-import com.jobinow.model.entities.Profil;
+import com.jobinow.model.entities.*;
 import com.jobinow.model.mapper.ProfilMapper;
+import com.jobinow.repositories.OfferRepository;
 import com.jobinow.repositories.ProfilRepository;
+import com.jobinow.services.spec.JobSeekerService;
 import com.jobinow.services.spec.ProfileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +15,23 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 /**
- * Service implementation for handling operations related to user profiles.
- * This service extends the generic service implementation {@link _ServiceImp}
- * and is specific to the {@link Profil} entity, providing methods for managing profiles.
-* */
+ * Implementation of the {@link JobSeekerService} interface for job seeker-related operations.
+ * This service provides methods for job seekers to apply to jobs, retrieve applied, saved, and recommended jobs,
+ * and filter jobs based on various criteria such as profile, location, salary range, job title, and company.
+ * <p>
+ * The class leverages the specified repository ({@link OfferRepository}) for data access and handles
+ * operations related to {@link Offer}, {@link Apply}, {@link User}, {@link Profil}, and {@link Company}.
+ * </p>
+ *
+ * @version 1.0
+ * @see JobSeekerService
+ * @see Offer
+ * @see Apply
+ * @see User
+ * @see Profil
+ * @see Company
+ * @see OfferRepository
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
