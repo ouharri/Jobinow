7c90df62c72ac2c4803557a2c90d6c9f96627690
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:31:50 +0000

    Implementation of the JobSeekerService interface for job seeker-related operations. This service provides methods for job seekers to apply to jobs, retrieve applied, saved, and recommended jobs, and filter jobs based on various criteria such as profile, location, salary range, job title, and company.

--- src/main/java/com/jobinow/services/impl/jobSeekerServiceImpl.java
+++ src/main/java/com/jobinow/services/impl/jobSeekerServiceImpl.java
@@ -5,15 +5,33 @@ import com.jobinow.model.entities.*;
 import com.jobinow.repositories.OfferRepository;
 import com.jobinow.services.spec.JobSeekerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Implementation of the {@link JobSeekerService} interface for job seeker-related operations.
+ * This service provides methods for job seekers to apply to jobs, retrieve applied, saved, and recommended jobs,
+ * and filter jobs based on various criteria such as profile, location, salary range, job title, and company.
+ * <p>
+ * The class leverages the specified repository ({@link OfferRepository}) for data access and handles
+ * operations related to {@link Offer}, {@link Apply}, {@link User}, {@link Profil}, and {@link Company}.
+ * </p>
+ *
+ * @version 1.0
+ * @see JobSeekerService
+ * @see Offer
+ * @see Apply
+ * @see User
+ * @see Profil
+ * @see Company
+ * @see OfferRepository
  */
-
+@Slf4j
+@Service
 @RequiredArgsConstructor
-public class jobSeekerServiceImpl implements JobSeekerService{
+public class jobSeekerServiceImpl implements JobSeekerService {
     private final OfferRepository OfferRepository;
 
     /**
@@ -35,7 +53,8 @@ public class jobSeekerServiceImpl implements JobSeekerService{
     @Override
     public List<Offer> getAppliedJobs(User jobSeeker) {
         List<Offer> appliedJobs = OfferRepository.findJobSeekerAppliedToOffers(jobSeeker.getId());
-        if (appliedJobs.isEmpty()) throw new ResourceNotFoundException("Job Seeker haven't applied to any offers with id " + jobSeeker.getId());
+        if (appliedJobs.isEmpty())
+            throw new ResourceNotFoundException("Job Seeker haven't applied to any offers with id " + jobSeeker.getId());
         return appliedJobs;
     }
 
