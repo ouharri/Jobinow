f05f2367befabefa10821f1388a388ea2fea0e1c
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:25:23 +0000

    Service interface for managing job offers. This service extends the generic service interface _Service, specializing in operations related to job offer entities. It defines methods for creating, retrieving, and handling other aspects of job offer data. The interface is designed to work with UUID as the identifier type for job offer entities.

--- src/main/java/com/jobinow/services/spec/OfferService.java
+++ src/main/java/com/jobinow/services/spec/OfferService.java
@@ -5,5 +5,20 @@ import com.jobinow.model.dto.responses.OfferResponse;
 
 import java.util.UUID;
 
+/**
+ * Service interface for managing job offers.
+ * This service extends the generic service interface {@link _Service},
+ * specializing in operations related to job offer entities.
+ * It defines methods for creating, retrieving, and handling
+ * other aspects of job offer data.
+ * <p>
+ * The interface is designed to work with UUID as the identifier type for job offer entities.
+ * </p>
+ *
+ * @version 1.0
+ * @see OfferRequest
+ * @see OfferResponse
+ * @see _Service
+ */
 public interface OfferService extends _Service<UUID, OfferRequest, OfferResponse> {
 }
