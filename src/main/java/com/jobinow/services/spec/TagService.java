0a012342b5c034ba566727720504d29346724f5f
ouharri <ouharrioutman@gmail.com>
2024-01-07 17:25:47 +0000

    Service interface for managing tags. This service extends the generic service interface _Service, specializing in operations related to tag entities. It defines methods for creating, retrieving, and handling other aspects of tag data. The interface is designed to work with UUID as the identifier type for tag entities.

--- src/main/java/com/jobinow/services/spec/TagService.java
+++ src/main/java/com/jobinow/services/spec/TagService.java
@@ -5,5 +5,20 @@ import com.jobinow.model.dto.responses.TagResponse;
 
 import java.util.UUID;
 
+/**
+ * Service interface for managing tags.
+ * This service extends the generic service interface {@link _Service},
+ * specializing in operations related to tag entities.
+ * It defines methods for creating, retrieving, and handling
+ * other aspects of tag data.
+ * <p>
+ * The interface is designed to work with UUID as the identifier type for tag entities.
+ * </p>
+ *
+ * @version 1.0
+ * @see TagRequest
+ * @see TagResponse
+ * @see _Service
+ */
 public interface TagService extends _Service<UUID, TagRequest, TagResponse> {
 }
